427cfb5818acf50a966230f9cf1a7956df50b1ed
austinj33 <164961440+austinj33@users.noreply.github.com>
2024-04-26 18:26:26 +0000

    finished

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,7 +4,27 @@ class DirectorsController < ApplicationController
   end
 
   def show
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => the_id })
+
+    @the_director = matching_records.first
+
     render({ :template => "director_templates/details"})
+  end
+
+  def junior
+    matching_directors = Director.order({ :dob => :desc })
+    @the_youngest = matching_directors.first
+  
+    render({ :template => "director_templates/youngest" })
+  end
+
+  def grandpa
+    matching_directors = Director.where.not(dob: nil).order({ :dob => :asc })
+    @the_eldest = matching_directors.first
+  
+    render({ :template => "director_templates/eldest" })
   end
   
 end
